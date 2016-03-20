b48ddc5c39fd7fc83be1c705b322a6d0a43b594e
lynnewallace <lwallace890@gmail.com>
2016-03-20 22:01:08 +0000

    Port to angular js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+var app = angular.module('ngAdCarousel', []);
+app.controller('adCtrl', function ($scope) {
+  $scope.images = ['DoubleHeader.jpg', 'FM.jpg', 'SOAK5.jpg'];
+});
+
 $('.carousel').carousel({
   interval: 10000
 })
